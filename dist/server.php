91f39a6c3f38ec68ffa2255ce651476a2ac1372c
Oleg <info@action-studio.ru>
2020-03-11 15:07:57 +0500

    add phpmail service

--- dist/server.php
+++ dist/server.php
@@ -1,2 +1,30 @@
 <?php
-echo var_dump($_POST);
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$NAME = isset($_POST['text']) ? $_POST['text'] : '';
+$MAILFROM = isset($_POST['email']) ? $_POST['email'] : '';
+
+$mail->isSMTP();
+$mail->Host = 'smtp.yandex.ru';
+$mail->SMTPAuth = true;
+$mail->Username = 'info@action-agency.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'LA4r5B32a'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';
+$mail->Port = 465;
+
+$mail->setFrom('info@action-agency.ru'); // от кого будет уходить письмо?
+$mail->addAddress('info@action-agency.ru');     // Кому будет уходить письмо 
+
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Хотят связаться';
+$mail->Body    = $NAME . ' оставил заявку<br>Почта этого пользователя: ' . $MAILFROM;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+  throw new Exception('Что-то пошло не так');
+} else {
+  echo "Email successful";  
+}
