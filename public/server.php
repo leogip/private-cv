6824eef25b304333794944708b9f942f1018245d
Oleg <info@action-studio.ru>
2020-03-19 22:18:01 +0500

    add settings mail

--- public/server.php
+++ public/server.php
@@ -9,13 +9,13 @@ $MAILFROM = isset($_POST['email']) ? $_POST['email'] : '';
 $mail->isSMTP();
 $mail->Host = 'smtp.yandex.ru';
 $mail->SMTPAuth = true;
-$mail->Username = 'info@action-agency.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'LA4r5B32a'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'feedback@bolshovdev.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = '2711112Leo'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';
 $mail->Port = 465;
 
-$mail->setFrom('info@action-agency.ru'); // от кого будет уходить письмо?
-$mail->addAddress('info@action-agency.ru');     // Кому будет уходить письмо 
+$mail->setFrom('feedback@bolshovdev.ru'); // от кого будет уходить письмо?
+$mail->addAddress('feedback@bolshovdev.ru');     // Кому будет уходить письмо 
 
 $mail->isHTML(true);                                  // Set email format to HTML
 
